f3cb3d825419993aa46f01f1eca58bce152c38e5
kelicaleb <kelicaleb7@gmail.com>
2023-01-01 20:08:56 +0300

    Completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Cat{
+    constructor(name, gender){
+        this.name = name 
+        this.gender = gender;
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+let cat = new Cat("Sasha", 'female');
+console.log(cat.speak())
+
+class Dog{
+    constructor(name, gender){
+        this.name = name ;
+        this.gender = gender ;
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+class Bird{
+    constructor(name, gender){
+        this.name = name ;
+        this.gender = gender;
+    }
+    speak(){
+        if(this.name == "Pablo"){
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else if(this.name == "Mable"){
+            return `${this.name} says squawk!`
+        }
+    }
+   
+}
